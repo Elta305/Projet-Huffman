e0cbebfd1d11c4cfdee3bc17b85f3346b77d5941
Elta305 <71965677+Elta305@users.noreply.github.com>
2023-04-03 22:09:46 +0200

    transformation en classe

--- fichiers.py
+++ fichiers.py
@@ -1,97 +1,123 @@
 import decryptage as d
-from cryptage import Huffman
+import cryptage as c
 
+class Fichier:
+    def __init__(self, f, c, t):
+        self.fichier = f
+        self.cle = c
+        self.texte_encode = t
+    
+    # @property
+    # def fichier(self):
+    #     return self.fichier
+    
+    # @property
+    # def cle(self):
+    #     return self.cle
+    
+    # @property
+    # def texte_encode(self):
+    #     return self.texte_encode
+    
+    def set_fichier(self, fichier):
+        self.fichier = fichier
 
-def ouverture_fichier(fichier):
-    """ str -> file
-    Ouvre un fichier texte.
-    """
-
-    fichier = open(fichier, "r", encoding='utf8')
-    return fichier
-
-
-def table_encodage(fichier):
-    """ str -> tab
-    Renvoie un tableau avec tous les éléments d'un fichier texte codés selon l'encodage de Huffman.
-    """
-
-    texte = ouverture_fichier(fichier)
-    texte = texte.read()
-
-    t = Huffman(texte)
-    tab = t.occurence()
-
-    abr = t.creation_arbre(tab)
-    tab = t.creation_table_encodage(abr)
-
-    return tab
-
-
-def compression_char(fichier):
-    """ str -> tab
-    Renvoie un tableau des caractères d'un texte codés selon l'encodage de Huffman.
-    """
-
-    tab = table_encodage()
-    tab2 = []
-    texte = ouverture_fichier(fichier)
-    texte = texte.read()
-    for lettre in texte:
-        for i in range(len(tab)):
-            if lettre == tab[i][0]:
-                tab2 += tab[i][1]
-    return tab2
-
-
-def compression_texte():
-    """ -> str
-    Renvoie un string des caractères d'un texte codés selon l'encodage de Huffman."""
-
-    tab = compression_char()
-    encodage = str()
-    for k in range(len(tab)):
-        encodage += str(tab[k])
-    return encodage
-
-
-def enregistrement_encodage():
-    """Enregistre le fichier texte encodé."""
-
-    texte = open("fichier_encode.txt", "w")
-    a = compression_texte()
-    texte.write(a)
-    texte.close()
-    texte = open("cle_encodage.txt", "w", encoding='utf8')
-    tab = str(table_encodage())
-    texte.write(tab)
-    texte.close()
-
-
-def decodage_texte(fichier_a_decoder, cle):
-    """ str, tab -> tab
-    Renvoie un tableau avec tous les éléments d'un fichier texte décodés.
-    """
+    def set_cle(self, cle):
+        self.cle = cle
 
     def set_texte_encode(self, texte_encode):
-        self.texte_encode = open(texte_encode, "r", encoding='utf8').read()
+        self.texte_encode = texte_encode
 
+    # def ouverture_fichier(self, fichier):
+    #     """ str -> file
+    #     Ouvre un fichier texte.
+    #     """
+        
+    #     fichier = open(fichier, "r", encoding = 'utf8')
+    #     return fichier
 
-def decompression_texte():
-    """ -> str
-    Renvoie un texte décodé."""
+    def table_encodage(self):
+        """ str -> tab
+        Renvoie un tableau avec tous les éléments d'un fichier texte codés selon l'encodage de Huffman.
+        """
+        
+        texte = self.fichier
+        t = c.Huffman(texte)
+        tab = t.occurence()
+        abr = t.creation_arbre(tab)
+        self.cle = t.creation_table_encodage(abr)
 
-    tab = decodage_texte()
-    decodage = str()
-    for k in range(len(tab)):
-        decodage += str(tab[k])
-    return decodage
+    def compression_char(self):
+        """ str -> tab
+        Renvoie un tableau des caractères d'un texte codés selon l'encodage de Huffman.
+        """
 
+        texte = self.fichier
+        tab = self.table_encodage()
+        tab2 = []
+        for lettre in texte:
+            for i in range(len(tab)):
+                if lettre == tab[i][0]:
+                    tab2 += tab[i][1]
+        return tab2
 
-def enregistrement_decodage():
-    """Enregistre le fichier texte décodé."""
+    def compression_texte(self):
+        """ -> str
+        Renvoie un string des caractères d'un texte codés selon l'encodage de Huffman."""
+        
+        tab = self.compression_char()
+        encodage = str()
+        for k in range(len(tab)):
+            encodage += str(tab[k])
+        return encodage
 
-    texte = open("fichier_decode.txt", "w", encoding='utf8')
-    a = decompression_texte()
-    texte.write(a)
-    texte.close()
+    def enregistrement_encodage(self):
+        """Enregistre le fichier texte encodé."""
+        
+        texte = open("fichier_encode.txt", "w")
+        a = self.compression_texte()
+        texte.write(a)
+        texte.close()
+        return a
+
+    def enregistrement_cle(self):
+        """Enregistre la clé de l'encodage de Huffman."""
+        
+        texte = open("cle_encodage.txt", "w", encoding='utf8')
+        tab = str(self.table_encodage())
+        texte.write(tab)
+        texte.close()
+        return tab
+
+    def decodage_texte(self):
+        """ str, tab -> tab
+        Renvoie un tableau avec tous les éléments d'un fichier texte décodés.
+        """
+
+        tmp = []
+        tab2 = []
+        for code in self.texte_encode:
+            tmp.append(int(code))
+            a = d.decodage_char(self.cle, tmp)
+            if a is not None:
+                tab2.append(a)
+                tmp = []
+        return tab2
+
+    def decompression_texte(self):
+        """ -> str
+        Renvoie un texte décodé."""
+        
+        tab = self.decodage_texte()
+        decodage = str()
+        for k in range(len(tab)):
+            decodage += str(tab[k])
+        return decodage
+
+    def enregistrement_decodage(self):
+        """Enregistre le fichier texte décodé."""
+        
+        texte = open("fichier_decode.txt", "w", encoding='utf8')
+        a = self.decompression_texte()
+        texte.write(a)
+        texte.close()
