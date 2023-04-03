d144977b0701224202af792413ed5d73feb94299
Elta305 <71965677+Elta305@users.noreply.github.com>
2023-04-03 13:54:31 +0200

    Add files via upload

--- Projet.py
+++ Projet.py
@@ -3,6 +3,7 @@ import tkinter.messagebox
 import tkinter.filedialog
 import customtkinter
 import fichiers as f
+import affichage as a
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -21,31 +22,35 @@ class App(customtkinter.CTk):
 
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=5, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(5, weight=1)
+        self.sidebar_frame.grid_rowconfigure(7, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Charger fichier", command=self.load_file)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Charger clé", command=self.load_key)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Enregistrer fichier", command=self.enregistrer_fichier)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Charger fichier encodé", command=self.load_encoded_file)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, text="Enregistrer clé", command=self.enregistrer_cle)
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, text="Enregistrer fichier", command=self.enregistrer_fichier)
         self.sidebar_button_4.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_5 = customtkinter.CTkButton(self.sidebar_frame, text="Enregistrer clé", command=self.enregistrer_cle)
+        self.sidebar_button_5.grid(row=5, column=0, padx=20, pady=10)
+        self.sidebar_button_6 = customtkinter.CTkButton(self.sidebar_frame, text="Enregistrer fichier encodé", command=self.enregistrer_fichier_encode)
+        self.sidebar_button_6.grid(row=6, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=8, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=9, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=8, column=0, padx=20, pady=(10, 0))
+        self.scaling_label.grid(row=10, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=9, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=11, column=0, padx=20, pady=(10, 20))
 
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Encoder")
+        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Encoder", command=self.encode)
         self.main_button_1.grid(row=0, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-        self.main_button_2 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Afficher Arbre")
+        self.main_button_2 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Afficher Arbre", command=self.affichage_arbre)
         self.main_button_2.grid(row=1, column=3, rowspan=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         self.textbox_encode = customtkinter.CTkTextbox(self, width=250, height=50)
@@ -61,6 +66,9 @@ class App(customtkinter.CTk):
         self.textbox.insert("0.0", "Clé de décodage")
         self.textbox_2.insert("0.0", "Texte encodé")
 
+        self.fichier = f.Fichier()
+        self.arbre = a.Interface()
+
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
@@ -71,22 +79,46 @@ class App(customtkinter.CTk):
     def load_file(self):
         ftypes = [('Text files', '*.txt'), ('All files', '*')]
         file_path = tkinter.filedialog.Open(self, filetypes=ftypes).show()
-        texte = f.ouverture_fichier(file_path).read()
+        texte = open(file_path, "r", encoding = 'utf8').read()
         self.textbox_encode.delete("0.0", "end")
         self.textbox_encode.insert("0.0", texte)
+        self.fichier.set_fichier(texte)
     
     def load_key(self):
         ftypes = [('Text files', '*.txt'), ('All files', '*')]
-        file_path = tkinter.filedialog.Open(self, filetypes=ftypes).show()
-        cle = f.ouverture_fichier(file_path).read()
+        key_path = tkinter.filedialog.Open(self, filetypes=ftypes).show()
+        cle = open(key_path, "r", encoding = 'utf8').read()
         self.textbox.delete("0.0", "end")
         self.textbox.insert("0.0", cle)
+        self.fichier.set_cle(cle)
+        
+    def load_encoded_file(self):
+        ftypes = [('Text files', '*.txt'), ('All files', '*')]
+        file_path = tkinter.filedialog.Open(self, filetypes=ftypes).show()
+        texte_encode = open(file_path, "r", encoding = 'utf8').read()
+        self.textbox_encode.delete("0.0", "end")
+        self.textbox_encode.insert("0.0", texte_encode)
+        self.fichier.set_texte_encode(texte_encode)
+ 
+    def encode(self):
+        texte_encode = self.fichier.enregistrement_encodage(self.textbox_encode.get("0.0", "end"))
+        texte_cle = self.fichier.enregistrement_cle(self.textbox_encode.get("0.0", "end"))
+        self.textbox.delete("0.0", "end")
+        self.textbox.insert("0.0", texte_cle)
+        self.textbox_2.delete("0.0", "end")
+        self.textbox_2.insert("0.0", texte_encode)
 
     def enregistrer_fichier(self):
-        f.enregistrement_encodage(self.textbox_2.get("0.0", "end"))
-    
+        self.fichier.enregistrement_encodage(self.textbox_2.get("0.0", "end"))
+
     def enregistrer_cle(self):
-        f.enregistrement_cle(self.textbox.get("0.0", "end"))
+        self.fichier.enregistrement_cle(self.textbox.get("0.0", "end"))
+    
+    def enregistrer_fichier_encode(self):
+        self.fichier.enregistrement_texte_encode(self.textbox_encode.get("0.0", "end"))
+
+    def affichage_arbre(self):
+        a.afficher_arbre()
     
 if __name__ == "__main__":
     app = App()
--- fichiers.py
+++ fichiers.py
@@ -1,94 +1,123 @@
 import decryptage as d
 
-def ouverture_fichier(fichier):
-    """ str -> file
-    Ouvre un fichier texte.
-    """
+class Fichier:
+    def __init__(self):
+        self.fichier = None
+        self.cle = None
+        self.texte_encode = None
     
-    fichier = open(fichier, "r", encoding = 'utf8')
-    return fichier
-
-def table_encodage(fichier):
-    """ str -> tab
-    Renvoie un tableau avec tous les éléments d'un fichier texte codés selon l'encodage de Huffman.
-    """
+    # @property
+    # def fichier(self):
+    #     return self.fichier
     
-    texte = ouverture_fichier(fichier)
-    texte = texte.read()
-    # tab = nuk(texte)
-    # abr = creation_arbre(tab)
-    # tab = encodage(tab, abr)
-    # return tab
-
-def compression_char(fichier):
-    """ str -> tab
-    Renvoie un tableau des caractères d'un texte codés selon l'encodage de Huffman.
-    """
-
-    tab = table_encodage()
-    tab2 = []
-    texte = ouverture_fichier(fichier)
-    texte = texte.read()
-    for lettre in texte:
-        for i in range(len(tab)):
-            if lettre == tab[i][0]:
-                tab2 += tab[i][1]
-    return tab2
-
-def compression_texte():
-    """ -> str
-    Renvoie un string des caractères d'un texte codés selon l'encodage de Huffman."""
+    # @property
+    # def cle(self):
+    #     return self.cle
     
-    tab = compression_char()
-    encodage = str()
-    for k in range(len(tab)):
-        encodage += str(tab[k])
-    return encodage
-
-def enregistrement_encodage():
-    """Enregistre le fichier texte encodé."""
+    # @property
+    # def texte_encode(self):
+    #     return self.texte_encode
     
-    texte = open("fichier_encode.txt", "w")
-    a = compression_texte()
-    texte.write(a)
-    texte.close()
-    texte = open("cle_encodage.txt", "w", encoding='utf8')
-    tab = str(table_encodage())
-    texte.write(tab)
-    texte.close()
+    def set_fichier(self, fichier):
+        self.fichier = open(fichier, "r", encoding = 'utf8').read()
 
-def decodage_texte(fichier_a_decoder, cle):
-    """ str, tab -> tab
-    Renvoie un tableau avec tous les éléments d'un fichier texte décodés.
-    """
+    def set_cle(self, cle):
+        self.cle = open(cle, "r", encoding = 'utf8').read()
 
-    tab = cle
-    tmp = []
-    tab2 = []
-    texte_encode = ouverture_fichier(fichier_a_decoder)
-    texte_encode = texte_encode.read()
-    for code in texte_encode:
-        tmp.append(int(code))
-        a = d.decodage_char(tab, tmp)
-        if a is not None:
-            tab2.append(a)
-            tmp = []
-    return tab2
+    def set_texte_encode(self, texte_encode):
+        self.texte_encode = open(texte_encode, "r", encoding = 'utf8').read()
 
-def decompression_texte():
-    """ -> str
-    Renvoie un texte décodé."""
-    
-    tab = decodage_texte()
-    decodage = str()
-    for k in range(len(tab)):
-        decodage += str(tab[k])
-    return decodage
+    # def ouverture_fichier(self, fichier):
+    #     """ str -> file
+    #     Ouvre un fichier texte.
+    #     """
+        
+    #     fichier = open(fichier, "r", encoding = 'utf8')
+    #     return fichier
 
-def enregistrement_decodage():
-    """Enregistre le fichier texte décodé."""
-    
-    texte = open("fichier_decode.txt", "w", encoding='utf8')
-    a = decompression_texte()
-    texte.write(a)
-    texte.close()
+    def table_encodage(self):
+        """ str -> tab
+        Renvoie un tableau avec tous les éléments d'un fichier texte codés selon l'encodage de Huffman.
+        """
+        
+        texte = self.fichier
+        # A implémenter, voir avec Kaan
+        # tab = nuk(texte)
+        # abr = creation_arbre(tab)
+        # tab = encodage(tab, abr)
+        # return tab
+
+    def compression_char(self):
+        """ str -> tab
+        Renvoie un tableau des caractères d'un texte codés selon l'encodage de Huffman.
+        """
+
+        texte = self.fichier
+        tab = self.table_encodage()
+        tab2 = []
+        for lettre in texte:
+            for i in range(len(tab)):
+                if lettre == tab[i][0]:
+                    tab2 += tab[i][1]
+        return tab2
+
+    def compression_texte(self):
+        """ -> str
+        Renvoie un string des caractères d'un texte codés selon l'encodage de Huffman."""
+        
+        tab = self.compression_char()
+        encodage = str()
+        for k in range(len(tab)):
+            encodage += str(tab[k])
+        return encodage
+
+    def enregistrement_encodage(self):
+        """Enregistre le fichier texte encodé."""
+        
+        texte = open("fichier_encode.txt", "w")
+        a = self.compression_texte()
+        texte.write(a)
+        texte.close()
+        return a
+
+    def enregistrement_cle(self):
+        """Enregistre la clé de l'encodage de Huffman."""
+        
+        texte = open("cle_encodage.txt", "w", encoding='utf8')
+        tab = str(self.table_encodage())
+        texte.write(tab)
+        texte.close()
+        return tab
+
+    def decodage_texte(self):
+        """ str, tab -> tab
+        Renvoie un tableau avec tous les éléments d'un fichier texte décodés.
+        """
+
+        tmp = []
+        tab2 = []
+        for code in self.texte_encode:
+            tmp.append(int(code))
+            a = d.decodage_char(self.cle, tmp)
+            if a is not None:
+                tab2.append(a)
+                tmp = []
+        return tab2
+
+    def decompression_texte(self):
+        """ -> str
+        Renvoie un texte décodé."""
+        
+        tab = self.decodage_texte()
+        decodage = str()
+        for k in range(len(tab)):
+            decodage += str(tab[k])
+        return decodage
+
+    def enregistrement_decodage(self):
+        """Enregistre le fichier texte décodé."""
+        
+        texte = open("fichier_decode.txt", "w", encoding='utf8')
+        a = self.decompression_texte()
+        texte.write(a)
+        texte.close()
